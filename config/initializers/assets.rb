a0918ab1ff1872cd2cec3bc09cdea3d505e4c2f2
kenigbolo <kenigbol@ut.ee>
2016-10-15 16:42:18 +0300

    Modify main.css to use correct assets path for homepage

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,13 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join('/app/assets/fonts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( main.css )
+Rails.application.config.assets.precompile += %w( tether.min.js )
+Rails.application.config.assets.precompile += %w( select.min.js )
+Rails.application.config.assets.precompile += %w( drop.min.js )
+Rails.application.config.assets.precompile += %w( main.js )
