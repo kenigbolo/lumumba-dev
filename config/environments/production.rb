1b43ce1b3378cb227dec13f3595035cf2c7a0bc7
kenigbolo <kenigbol@ut.ee>
2016-10-16 18:32:44 +0300

    Modify production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,11 +88,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {:host => 'lumumba.heroku.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.sendgrid.net",
-    :port    => 587,
-    :domain  => 'heroku.com'
-    :user_name => ENV['SENDGRID_USERNAME']
-    :password => ENV['SENDGRID_PASSWORD']
+    address: "smtp.sendgrid.net",
+    port: '587',
+    domain: 'heroku.com',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    enable_starttls_auto: true
   }
   
   # ActionMailer::Base.delivery_method = :smtp
