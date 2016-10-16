812e24a2bbd15e476dd3407754d0f2b13a2146f9
kenigbolo <kenigbol@ut.ee>
2016-10-16 19:11:00 +0300

    Modify mailing defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = {:host => 'lumumba.heroku.com'}
+  # config.action_mailer.default_url_options = {:host => 'lumumba.heroku.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
@@ -93,6 +93,7 @@ Rails.application.configure do
     domain: 'heroku.com',
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
+    authentication: :plain,
     enable_starttls_auto: true
   }
   
