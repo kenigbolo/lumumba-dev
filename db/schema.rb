b611e66692ed2600a097a1848c91299534b32afd
kenigbolo <kenigbol@ut.ee>
2016-10-17 14:40:30 +0300

    close #6 - Allow users to upload pictures of their choice

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016110620) do
+ActiveRecord::Schema.define(version: 20161017110621) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20161016110620) do
     t.string   "phone_number"
     t.string   "description"
     t.string   "slug"
+    t.string   "image_status"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
