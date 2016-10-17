e85671e3cfbf8d64dddb9207584cf7000d2bf581
kenigbolo <kenigbol@ut.ee>
2016-10-17 12:47:07 +0300

    Redirect user to user profile after user authenticates via facebook

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,9 +3,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
-      flash["notice"] = "Signed In"
+      flash["notice"] = "Authenticated via facebook"
       sign_in user
-      redirect_to :controller=>"home", :action=>"index"
+      redirect_to :controller=>"users", :action=>"show", id: user.slug
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
