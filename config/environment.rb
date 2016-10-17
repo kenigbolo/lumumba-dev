f7a13aecab871c9d05aa248b7c47ea4ca67aa7bd
kenigbolo <kenigbol@ut.ee>
2016-10-17 15:51:07 +0300

    Fix carrierwave issue in production

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
-require_relative 'application'
 require 'carrierwave/orm/activerecord'
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
