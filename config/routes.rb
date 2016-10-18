07ca70280e818a2e0b37bc3dd4a65c890018fcea
kenigbolo <kenigbol@ut.ee>
2016-10-18 13:09:31 +0300

    Add billing information frontend and modify javascript for click functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks", registrations: 'registrations', confirmations: "confirmations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
