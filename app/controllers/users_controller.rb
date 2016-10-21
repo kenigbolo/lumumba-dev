60570e86503e69308a105196279402647156f57f
kenigbolo <kenigbol@ut.ee>
2016-10-21 17:27:12 +0300

    Authenticate users when trying to view profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def show
   	@user = User.friendly.find(params[:id])
+  	@designs = Design.where("user_id = ?", @user.id)
   	if current_user.id == @user.id
   	  @page = "settings"
   	else
