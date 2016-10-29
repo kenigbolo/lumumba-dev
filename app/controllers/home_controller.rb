c058bccb6adafc4b33718aa0ce09496b9dad4972
kenigbolo <kenigbol@ut.ee>
2016-10-30 02:36:21 +0300

    Add leaderboard method and update frontent-end to reflect leaderboard dynamically

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,8 @@ class HomeController < ApplicationController
   def index
   end
   
+  def leaderboard
+  	designs = Design.where("for_competition = ?", true)
+  	@designs = designs.first(10).sort{ |designA, designB| designB.votes_for.size <=> designA.votes_for.size}
+  end
 end
