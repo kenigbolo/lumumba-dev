018f72703995d9cc53ae61282b730fe7fd459bd4
kenigbolo <kenigbol@ut.ee>
2016-11-01 00:08:05 +0200

    Make modifications to front-end code and remove redundant links

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -4,6 +4,7 @@ class OrderItemsController < ApplicationController
 		item = OrderItem.new(order_item_params)
 		order = current_user.orders.where("status = ?", "open").first
 		amount = Product.find(params[:product_id]).price * item.quantity
+
 		if order.nil?
 			new_order = current_user.orders.create(status: "open")
 			item_order_save(item, new_order, amount)
@@ -18,7 +19,8 @@ class OrderItemsController < ApplicationController
 	  if current_user.id == order_item.order.user.id
 	  	order = current_user.orders.where("id = ?", order_item.order_id).first
 	  	order.sub_total -= (order_item.get_product.price * order_item.quantity)
-	  	order.save && order_item.destroy
+	  	order.save
+	  	order_item.destroy
 	  	flash["notice"] = "Item successfully deleted from Cart"
 	  else
 	  	flash["notice"] = "You do not have the permission to delete this post"	 
@@ -36,10 +38,11 @@ class OrderItemsController < ApplicationController
 			item.order_id = order.id
 			item.product_id = params[:product_id]
 			order.sub_total += amount
-			order.save && item.save
+			item.save
 
 			if item.persisted?
 				flash["notice"] = "Item successfully added to cart"
+				order.save
 			else
 				flash["notice"] = "Item could not be added to your cart, please try again"
 			end
