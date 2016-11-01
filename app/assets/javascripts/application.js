5e0dee28bbc2123310edac378777ebc001e1b8d8
kenigbolo <kenigbol@ut.ee>
2016-11-01 12:11:33 +0200

    #53 Fix bug in state display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,15 +30,4 @@ $(document).ready(function(){
 	  $('#new_address_form').show();
 	});			
 
-	$(function() {
-	  return $('select#address_country').change(function(event) {
-	    var country, select_wrapper, url;
-	    select_wrapper = $('#address_state_wrapper');
-	    $('select', select_wrapper).attr('disabled', true);
-	    country = $(this).val();
-	    url = "/address/subregion_options?parent_region=" + country;
-	    return select_wrapper.load(url);
-	  });
-	});
-	   
 });
