2479641191e83eba218028f54a68a56ca7d7bc0a
vemv <vemv@vemv.net>
2016-11-01 21:30:08 +0100

    Make existing factories/specs pass

--- spec/factories/designs.rb
+++ spec/factories/designs.rb
@@ -1,23 +1,33 @@
 FactoryGirl.define do
   factory :design do
-    image "MyString"
-    image_desc "MyString"
-    first_garment_model_design "MyString"
-    first_garment_print_design "MyString"
-    first_garment_technical_design "MyString"
-    first_garment_desc "MyString"
-    second_garment_model_design "MyString"
-    second_garment_print_design "MyString"
-    second_garment_technical_design "MyString"
-    second_garment_desc "MyString"
-    third_garment_model_design "MyString"
-    third_garment_print_design "MyString"
-    third_garment_technical_design "MyString"
+
+    image { any_image }
+    image_desc { SecureRandom.hex }
+
+    first_garment_design { any_image }
+    first_garment_model_design { any_image }
+    first_garment_print_design { any_image }
+    first_garment_technical_design { any_image }
+    first_garment_desc { SecureRandom.hex }
+
+    second_garment_design { any_image }
+    second_garment_model_design { any_image }
+    second_garment_print_design { any_image }
+    second_garment_technical_design { any_image }
+    second_garment_desc { SecureRandom.hex }
+
+    third_garment_design { any_image }
+    third_garment_model_design { any_image }
+    third_garment_print_design { any_image }
+    third_garment_technical_design { any_image }
+    third_garment_desc { SecureRandom.hex }
+
     for_competition false
     for_sale false
     price "9.99"
-    add_to "MyString"
+    add_to { SecureRandom.hex }
     competition false
     user nil
+
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,25 @@
 require 'capybara/rspec'
+
 RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation) # leave it clean for FactoryGirl
+    FactoryGirl.lint
+    DatabaseCleaner.clean_with(:truncation) # clean what FactoryGirl created
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
+
   end
+
   config.shared_context_metadata_behavior = :apply_to_host_groups
+end
+
+def any_image
+  Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'blank.png'))
 end
