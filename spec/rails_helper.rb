d1fa9b02ad5a824fcb57b45a0c4346522dbee0d0
vemv <vemv@users.noreply.github.com>
2016-11-13 20:18:29 +0100

    Don't hit Cloudinary from test env, and prevent similar issues with Webmock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,9 @@ require File.expand_path('../../config/environment', __FILE__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'webmock/rspec'
 ActiveRecord::Migration.maintain_test_schema!
+WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
